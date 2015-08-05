19f843c53f09c88c96c54aa621b4c7102ef59f0d
James Gomez <jim.gomez.48@gmail.com>
2015-08-05 02:38:59 -0700

    Started controller logic for resources. Just stubs really.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,31 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+    render json: @movies
+  end
+
+  def new
+    # TODO
+  end
+
+  def create
+    # TODO
+  end
+
+  def edit
+    # TODO
+  end
+
+  def show
+    @movies = Movie.all
+    render json: @movies
+  end
+
+  def update
+    # TODO
+  end
+
+  def destroy
+    # TODO
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   resources :reviews
   resources :directors
   resources :actors
   resources :movies
-  # You can have the root of your site routed with "root"
-  root 'home#index'
 
-  get 'home/index'
+  # get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
