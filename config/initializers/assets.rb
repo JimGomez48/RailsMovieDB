9cd59c86e7b00d54bf8992b7e1013b7693300114
James Gomez <jim.gomez.48@gmail.com>
2015-08-15 17:22:09 -0700

    changed star-rating from .css to .scss for consistency. Customized pagination style with params.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.precompile += %w( star-rating.min.css )
+Rails.application.config.assets.precompile += %w( star-rating.mins.scss )
 
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
