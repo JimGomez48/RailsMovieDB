576fe3aa5adb1590c97f54490c88382713063517
James Gomez <jim.gomez.48@gmail.com>
2015-08-27 19:58:56 -0700

    Moved footer and navbar into ng-app/core/components. Updated references as needed.

--- app/assets/javascripts/ng-app/components/core/footer/footer.dir.js
+++ app/assets/javascripts/ng-app/components/core/footer/footer.dir.js
@@ -1,6 +1,6 @@
 angular.module('movieDbApp').directive('footerSection', function() {
     return {
         restrict: 'E',
-        templateUrl: 'ng-app/shared/footer/_footer.html'
+        templateUrl: 'ng-app/components/core/footer/_footer.html'
     }
 });
--- app/assets/javascripts/ng-app/components/core/top_navbar/navbar.dir.js
+++ app/assets/javascripts/ng-app/components/core/top_navbar/navbar.dir.js
@@ -1,7 +1,7 @@
 angular.module('movieDbApp').directive('navbarSection', function() {
     return {
         restrict: 'E',
-        templateUrl: 'ng-app/shared/top_navbar/_navbar.html',
+        templateUrl: 'ng-app/components/core/top_navbar/_navbar.html',
         controller: 'NavbarCtrl',
     }
 });
