1da9bc63318a29c1b8018499f38b1de22bbadb8f
James Gomez <jim.gomez.48@gmail.com>
2015-08-31 02:46:01 -0700

    Passing relevant pagination data from API. Also, using angular-bootstrap pagination directive.

--- app/assets/javascripts/ng-app/components/movies/movies.ctrl.js
+++ app/assets/javascripts/ng-app/components/movies/movies.ctrl.js
@@ -1,26 +1,39 @@
 angular.module('movieDbApp')
     .controller('MoviesCtrl', [
+        '$log',
         '$scope',
+        '$rootScope',
         'Restangular',
         '$controller',
-        function ($scope, Restangular, $controller) {
-            //var PageCtrl = $controller('PaginatorCtrl', {});
-
-            $scope.$parent.panel_title = 'Movies';
-
-            $scope.$parent.panel_menu_items = [
+        function ($log, $scope, $rootScope, Restangular, $controller) {
+            // initialize title
+            $scope.$parent.panelTitle = 'Movies';
+            // initialize menu items
+            $scope.$parent.panelMenuItems = [
                 {title: 'action 1', action: '1'},
                 {title: 'action 2', action: '2'},
                 {title: 'action 3', action: '3'},
             ];
 
-            Restangular.all('movies').getList({page: 3}).then(function (movies) {
+            var PageCtrl = $controller('PaginatorCtrl', {$scope: $scope});
+
+            // initialize view to page 1 of movie content
+            Restangular.all('movies').getList({page: 1}).then(function (movies) {
                 $scope.movies = movies;
-                console.log(movies.pagination);
-                console.log('Current page: ' + movies.pagination['current_page']);
-                console.log('Total pages: ' + movies.pagination['total_pages']);
-                console.log('Total items: ' + movies.pagination['total_items']);
-                console.log('Items per page: ' + movies.pagination['items_per_page']);
+                $log.debug('Current page: ' + movies.pagination['current_page']);
+                $log.debug('Total pages: ' + movies.pagination['total_pages']);
+                $log.debug('Total items: ' + movies.pagination['total_items']);
+                $log.debug('Items per page: ' + movies.pagination['items_per_page']);
+                $scope.$parent.totalItems = movies.pagination['total_items'];
+                $scope.$parent.itemsPerPage = movies.pagination['items_per_page'];
+                $log.log(PageCtrl.totalItems);
             });
+
+            $rootScope.$on('PageChangedEvent', function (event, args) {
+                Restangular.all('movies').getList({page: args.page}).then(function (movies) {
+                    $scope.movies = movies;
+                });
+                $log.info('MOVIES: Page changed to ' + args.page);
+            })
         }
     ]);
--- app/assets/javascripts/ng-app/shared/paginator/paginator.dir.js
+++ app/assets/javascripts/ng-app/shared/paginator/paginator.dir.js
@@ -4,9 +4,9 @@ angular.module('movieDbApp').directive('paginator', function () {
         templateUrl: 'ng-app/shared/paginator/_paginator.html',
         controller: 'PaginatorCtrl',
         scope: {
-            current_page: '@',
-            total_items: '=',
-            items_per_page: '='
+            currentPage: '@',
+            totalItems: '=',
+            itemsPerPage: '='
         }
     };
 });
